e9012043c83247d16d6d5fb9fd603ae46790ab50
nicklassundin <p4cr@hotmail.com>
2022-11-29 15:57:20 +0100

    fix(remove promt): remove promts

--- module.quick.spec.js
+++ module.quick.spec.js
@@ -250,7 +250,7 @@ describe(
                         })
                     })
                 })
-                describe.only('baseline', () => {
+                describe('baseline', () => {
                     it('temperature', () => {
                         let params = ['temperature', 'yrly', 'baseline']
                         let config = Object.assign(configs['live'], specs)
